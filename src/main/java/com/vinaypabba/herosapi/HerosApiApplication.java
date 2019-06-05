b7257f07e9e1a0f71111e207ea28f3370bae9558
vinaynikhil313 <vinay.nikhil@gmail.com>
2019-06-05 18:24:10 +0530

    Created skeleton Spring Boot project

---
+++ src/main/java/com/vinaypabba/herosapi/HerosApiApplication.java
@@ -0,0 +1,13 @@
+package com.vinaypabba.herosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerosApiApplication.class, args);
+	}
+
+}
